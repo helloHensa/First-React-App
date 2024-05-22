35b0a685a567c453110a1358182d268a3643c535
Patryk Hensel <patrykhensa@gmail.com>
2024-05-22 21:09:39 +0200

    third

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Customized second
+          Customized third
         </p>
         <a
           className="App-link"
