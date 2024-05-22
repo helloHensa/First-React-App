c994b3ba099f314966594ad7c685a5fccd6ab0b0
Patryk Hensel <patrykhensa@gmail.com>
2024-05-22 21:05:02 +0200

    commt

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Customized
+          Customized second
         </p>
         <a
           className="App-link"
